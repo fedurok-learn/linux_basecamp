60ce0050f65ce8a9e5cfda6ddb4c066c289d2cf0
B1Z0N <kolausf@gmail.com>
2022-01-21 21:40:04 +0200

    Fixed shellcheck warnings.

--- homeworks/1/show_user_info_and_data.sh
+++ homeworks/1/show_user_info_and_data.sh
@@ -6,7 +6,7 @@ DIR_PATH="$2"
 ERROR_MESSAGE="Finding \"root\" user data is not allowed!"
 
 # 1.2.4.
-[[ "$USERNAME" -eq root ]] && echo "$ERROR_MESSAGE" 1>&2 && exit 42
+[[ "$USERNAME" = root ]] && echo "$ERROR_MESSAGE" 1>&2 && exit 42
 
 # 1.2.2.
 find "$DIR_PATH" -user "$USERNAME" | sed "s/\(.*\)/\1 is found!/"
